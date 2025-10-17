c7d06f44d09a15b1de5a8c8ce05034646c99049d
Anene Praise <anenepraise1@gmail.com>
2025-10-17 22:29:50 +0100

    feat: implement hover animation for buttons

--- src/components/CardButton.tsx
+++ src/components/CardButton.tsx
@@ -1,14 +1,36 @@
-import classes from "./CardButton.module.css"
+import classes from "./CardButton.module.css";
+import { motion, type Variants } from "motion/react";
 
 type ButtonProps = {
   buttonText: string;
   buttonLink: string;
 };
 
+const buttonVariant: Variants = {
+  initial: { backgroundColor: "#333333", transition: { duration: 0.5 } },
+  hover: { backgroundColor: "#c4f82a", transition: { duration: 0.5 } },
+};
+
+const textVariant: Variants = {
+  initial: { color: "#ffffff", transition: { duration: 0.5 } },
+  hover: { color: "#333333", transition: { duration: 0.5 } },
+};
+
 export default function CardButton({ buttonText, buttonLink }: ButtonProps) {
   return (
-    <button className={classes.button}>
-      <a href={buttonLink} className={classes["link-text"]}>{buttonText}</a>
-    </button>
+    <motion.button
+      className={classes.button}
+      variants={buttonVariant}
+      animate="initial"
+      whileHover="hover"
+    >
+      <motion.a
+        href={buttonLink}
+        className={classes["link-text"]}
+        variants={textVariant}
+      >
+        {buttonText}
+      </motion.a>
+    </motion.button>
   );
 }
